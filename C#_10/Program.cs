a062814263d9da6a3ca29177c95a07b4164030b8
Лилия Хачатрян <163654259+lh2024W@users.noreply.github.com>
2024-06-23 13:24:26 +0300

    Update Program.cs

--- C#_10/Program.cs
+++ C#_10/Program.cs
@@ -1,40 +1,32 @@
-﻿using System.Collections;
-using System.Threading;
+using System.Collections;
+
 namespace C__10
 {
-
     class Student : IComparable
     {
-        public string firstname;//имя
-        public string secondname;//отчество
-        public string lastname;//фамилия
-        public string birthday;//дата рождения
-        public string address;//домашний адрес
-        public string phone;//телефон
-        List<int> Tests;//зачеты
-        List<int> TermPapers;//курсовые работы
-        List<int> Exams;//экзамены
+        public string firstname; // имя
+        public string secondname; // отчество
+        public string lastname; // фамилия
+        public string birthday; // дата рождения
+        public string address; // домашний адрес
+        public string phone; // телефон
+        List<int> Tests = new List<int>(); // зачеты
+        List<int> TermPapers = new List<int>(); // курсовые работы
+        List<int> Exams = new List<int>(); // экзамены
 
         public Student() : this(10, 11, 11)
         {
-            //Console.WriteLine("C-tor without params");
+            // Console.WriteLine("C-tor without params");
         }
 
         public Student(int test, int term, int exam) : this("Иван", "Иванович", "Иванов", "12.01.2003",
             "г.Одесса ул.Пушкинская 10", "069635422", 12, 12, 12)
         {
-            Tests = new List<int>();
-            SetTests(test);
-            TermPapers = new List<int>();
-            SetTermPapers(term);
-            Exams = new List<int>();
-            SetExams(exam);
-
-            //Console.WriteLine("C-tor with params");
+            // здесь убрал тело конструктора, так как есть делегирование
         }
 
         public Student(string firstname, string secondname, string lastname, string birthday, string address, string phone,
-            int test, int term, int exam)//main c-tor
+            int test, int term, int exam) // main c-tor
         {
             SetFirstname(firstname);
             SetSecondname(secondname);
@@ -42,14 +34,9 @@ namespace C__10
             SetBirthday(birthday);
             SetAddress(address);
             SetPhone(phone);
-            Tests = new List<int>();
-            SetTests(test);
-            TermPapers = new List<int>();
-            SetTermPapers(term);
-            Exams = new List<int>();
-            SetExams(exam);
-
-            //Console.WriteLine("Main c-tor");
+            AddTestRate(test);
+            AddTermPapersRate(term);
+            AddExamRate(exam);
         }
 
         public void SetFirstname(string firstname)
@@ -82,17 +69,18 @@ namespace C__10
             this.phone = phone;
         }
 
-        public void SetTests(int test)
+        // переименовал названия методов на Add...Rate, так как по смыслу происходит именно это
+        public void AddTestRate(int test)
         {
             Tests.Add(test);
         }
 
-        public void SetTermPapers(int term)
+        public void AddTermPapersRate(int term)
         {
             TermPapers.Add(term);
         }
 
-        public void SetExams(int exam)
+        public void AddExamRate(int exam)
         {
             Exams.Add(exam);
         }
@@ -150,6 +138,7 @@ namespace C__10
             Console.WriteLine("Дата рождения: " + GetBirthday());
             Console.WriteLine("Адрес проживания: " + GetAddress());
             Console.WriteLine("Телефон: " + GetPhone());
+
             foreach (int test in Tests)
             {
                 Console.WriteLine("Оценки за зачеты: " + test);
@@ -167,8 +156,9 @@ namespace C__10
 
         public override string ToString()
         {
-            return firstname + " " + secondname + " " + lastname + " " + birthday + " " + address + " " + phone + " " +
-                Tests + " " + TermPapers + " " + Exams + " ";
+            // массивы оценок можно не учитывать при преобразовании в строку
+            return firstname + " " + secondname + " " + lastname + " " + birthday + " " + address + " " + phone;
+            // а если и учитывать, то без перебора фором/форычем не обойтись 
         }
 
         public void Study()
@@ -208,86 +198,107 @@ namespace C__10
             {
                 return firstname.CompareTo((obj as Student).firstname);
             }
-            throw new NotImplementedException();
+            // else
+            throw new Exception("реализовано сравнение только для 2 объектов типа Студент");
         }
 
+        // это компаратор для класса Array
         public class SortByName : IComparer
         {
             public int Compare(object ob1, object ob2)
             {
                 Student s1 = (Student)ob1;
                 Student s2 = (Student)ob2;
-                return String.Compare(s1.firstname, s2.firstname);
+
+                return string.Compare(s1.firstname, s2.firstname);
             }
         }
 
-        public class SortByLastname : IComparer
+        // это отдельный компаратор для поиска по имени
+        public class NameComparer : IComparer
         {
+            private string name;
+
+            public NameComparer(string name)
+            {
+                this.name = name;
+            }
+
             public int Compare(object ob1, object ob2)
             {
-                Student s1 = (Student)ob1;
-                Student s2 = (Student)ob2;
-                return String.Compare(s1.lastname, s2.lastname);
+                if (ob1 is Student student)
+                {
+                    return string.Compare(student.firstname, name);
+                }
+                throw new ArgumentException("Object1 is not a Student");
             }
-        }
-
-        public class SortByLengthAddress : IComparer
-        {
-            public int Compare(object ob1, object ob2)
-            {
-                Student s1 = (Student)ob1;
-                Student s2 = (Student)ob2;
-                if (s1.address.Length < s2.address.Length) return 1;
-                else return 0;
-            }
-        }
-
-        public int SearchByName(object obj, string name)// не могу проверить, не знаю что не так... 
-        {
-            if (obj is Student)
-            {
-                return name.CompareTo((obj as Student).firstname);
-            }
-            throw new NotImplementedException();
-            
         }
     }
 
-        class Program
+    public class SortByLastname : IComparer
+    {
+        public int Compare(object ob1, object ob2)
         {
-            static void Main(string[] args)
-            {
-                Student [] students = new Student[5];
-                students[0] = new Student("Тарас", "Григорьевич", "Шевченко", "24.03.2001", "г.Киев", "0502658985", 12, 12, 12);
-                students[1] = new Student("Александр", "Витальевич", "Савицкий", "21.11.1988", "г.Запорожье", "67559440026", 12, 11, 12);
-                students[2] = new Student("Ольга", "Юрьевна", "Корсар", "04.12.1984", "г.Киев", "09548565652", 11, 12, 12);
-                students[3] = new Student("Вадим", "Олегович", "Шустрый", "31.12.2010", "г.Одесса", "04852115253", 10, 12, 9);
-                students[4] = new Student("Анна", "Владимировна", "Борисенко", "03.08.1999", "г.Львов", "2659947436", 11, 11, 11);
-                
-                Array.Sort(students);
-
-                foreach (Student student in students)
-                {
-                    Console.WriteLine(student);//не показывает оценки
-                }
-                Console.WriteLine();
-
-                Array.Sort(students, new Student.SortByName());
-                foreach (Student student in students)
-                {
-                    Console.WriteLine(student);//не показывает оценки
-                }
-                Console.WriteLine();
-
-                Array.Sort(students, new Student.SortByLengthAddress());
-                foreach (Student student in students)
-                {
-                    Console.WriteLine(student);//не показывает оценки
-                }
-                Console.WriteLine();
-
-            
-            }
+            Student s1 = (Student)ob1;
+            Student s2 = (Student)ob2;
+            return string.Compare(s1.lastname, s2.lastname);
         }
-    
+    }
+
+    // метод поиска студента будет вне класса Студент, либо как метод Группы, либо как метод ещё какого-то класса
+    // public int SearchByName(object obj, string name)
+
+
+    class Program
+    {
+        static Student SearchStudent(Student[] collection, IComparer comparer)
+        {
+            foreach (var student in collection)
+            {
+                if (comparer.Compare(student, null) == 0)
+                {
+                    return student;
+                }
+            }
+            return null; // студент не найден
+        }
+
+        static void Main()
+        {
+            Student[] students =
+            [
+                new Student("Тарас", "Григорьевич", "Шевченко", "24.03.2001", "г.Киев", "0502658985", 12, 12, 12),
+                new Student("Александр", "Витальевич", "Савицкий", "21.11.1988", "г.Запорожье", "67559440026", 12, 11, 12),
+                new Student("Ольга", "Юрьевна", "Корсар", "04.12.1984", "г.Киев", "09548565652", 11, 12, 12),
+                new Student("Вадим", "Олегович", "Шустрый", "31.12.2010", "г.Одесса", "04852115253", 10, 12, 9),
+                new Student("Анна", "Владимировна", "Борисенко", "03.08.1999", "г.Львов", "2659947436", 11, 11, 11),
+            ];
+
+            Student result = SearchStudent(students, new Student.NameComparer("Тарас"));
+            Console.WriteLine(result);
+
+            /*Array.Sort(students);
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student); // не показывает оценки
+            }
+            Console.WriteLine();
+
+            Array.Sort(students, new Student.SortByName());
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student); // не показывает оценки
+            }
+            Console.WriteLine();
+
+            Array.Sort(students);
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student); // не показывает оценки
+            }
+            Console.WriteLine(); */
+        }
+    }
+
 }
